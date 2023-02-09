9e81711a9caf3445eb5e7d91461463ba938eccc8
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-02-09 21:29:41 +0500

    Add necessary imports and configure store

---
+++ src/redux/configureStore.js
@@ -0,0 +1,16 @@
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import rocketReducer from './rockets/rockets';
+
+const store = configureStore(
+  {
+    // Assign the rocketReducer to the rockets key in the store.
+    reducer: {
+      rockets: rocketReducer,
+    },
+  },
+  // Apply the thunk middleware to the store.
+  applyMiddleware((thunk)),
+);
+
+export default store;
