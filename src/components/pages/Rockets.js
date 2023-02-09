7f1ebafaf3904e46a8af5f4bcb8dc7137f30134f
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-02-09 21:33:32 +0500

    Implement Rockets componnt hooks & dispatch action

--- src/components/pages/Rockets.js
+++ src/components/pages/Rockets.js
@@ -1,5 +1,19 @@
-const Rockets = () => (
-  <div>Rockets</div>
-);
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchRockets } from '../../redux/rockets/rockets';
+
+const Rockets = () => {
+  // `useDispatch` hook to access the `dispatch` function from `react-redux`.
+  const dispatch = useDispatch();
+
+  // `useEffect` hook to dispatch the `fetchRockets` action when the component is mounted.
+  useEffect(() => {
+    dispatch(fetchRockets());
+  }, [dispatch]); // Added `dispatch` function as a dependency to re-run the effect when it changes.
+
+  return (
+    <div>Rockets</div>
+  );
+};
 
 export default Rockets;
