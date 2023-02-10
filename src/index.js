5f9c697683f161ef831927e3e487bb3bed0c8d22
Inayat-khan <khanetmoi@gmail.com>
2023-02-10 14:55:36 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={Store}>
       <App />
     </Provider>
   </React.StrictMode>,
