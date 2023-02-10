5dbb221b2f71595153cefa59e3280e3b3bd0929f
M.Nisar Ali <101400298+MNisarAli@users.noreply.github.com>
2023-02-10 16:42:07 +0500

    Fix lint errors after resolving merge conflict

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
-import Mission from './components/pages/Mission';
 import Rockets from './components/pages/Rockets';
+import Mission from './components/pages/Mission';
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <header className="App-header">
-          <Navigation />
-        </header>
-        <main>
-          <Routes>
-            <Route path="/" element={<Rockets />} />
-            <Route path="/missions" element={<Mission />} />
-          </Routes>
-        </main>
-      </BrowserRouter>
-    </>
-  );
-}
+const App = () => (
+  <>
+    <BrowserRouter>
+      <header className="App-header">
+        <Navigation />
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Rockets />} />
+          <Route path="/missions" element={<Mission />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  </>
+);
 
 export default App;
