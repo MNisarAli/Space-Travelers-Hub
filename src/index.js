135d1543aa67b043b95b8a6449d510b612d33125
Inayat-khan <khanetmoi@gmail.com>
2023-02-10 15:01:24 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-
-import store from './redux/configStore';
-import Store from './redux/configureStore';
+import store from './redux/configureStore';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={Store}>
+    <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
