ef713ca67e3a759753e1ae84bbec8657fc1f8ae4
Inayat-khan <khanetmoi@gmail.com>
2023-02-10 15:09:02 +0000

    Update configureStore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,14 @@
 import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import rocketReducer from './rockets/rockets';
+import missionsReducer from './missions/mission';
 
 const store = configureStore(
   {
     // Assign the rocketReducer to the rockets key in the store.
     reducer: {
       rockets: rocketReducer,
+      missions: missionsReducer,
     },
   },
   // Apply the thunk middleware to the store.
